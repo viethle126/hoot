4790f7522204d52baf83f9319a19fb0dafb008d8
Viet Le <viethle126@gmail.com>
2016-03-26 22:25:26 -0700

    Module random.js generates tweets for all users #1
    Generates 20-50 tweets for all users:
    * Contains 6-20 lorem ipsum words
    * Timestamps range from 01/01/16 12:01am to current time

---
+++ random.js
@@ -0,0 +1,103 @@
+var users = require('./users.js');
+// not implemented
+//var keys = [];
+//(function addKeys(array, obj) {
+//  for (prop in obj) {
+//    if (array.indexOf(prop) === -1) { array.push(prop) }
+//  }
+//  return array;
+//})(keys, users);
+
+function random() {
+  var ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci. Ut eu diam at pede suscipit sodales. Aenean lectus elit, fermentum non, convallis id, sagittis at, neque. Nullam mauris orci, aliquet et, iaculis et, viverra vitae, ligula. Nulla ut felis in purus aliquam imperdiet. Maecenas aliquet mollis lectus. Vivamus consectetuer risus et tortor.'.split(' ');
+  var now = new Date();
+  var nowDate = now.getDate();
+  var nowHours = now.getHours();
+  var nowMinutes = now.getMinutes();
+
+  function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  function cut(start, length) {
+    var content = [];
+    for (var i = start; i < start + length; i++) {
+      if (i > 379) {
+        content.push(ipsum[i - 380]);
+      } else {
+        content.push(ipsum[i]);
+      }
+    }
+    content = content.join(' ');
+    if (content[content.length - 1] === ',' || content[content.length - 1] === '.') {
+      content = content.slice(0, -1);
+    }
+    return capitalize(content + '.');
+  }
+
+  function content() {
+    var start = Math.floor(Math.random() * 380);
+    var length = Math.floor(Math.random() * 15) + 6;
+    return cut(start, length);
+  }
+
+  function month() {
+    return Math.floor(Math.random() * 3) + 1;
+    // change to * 4 when April starts
+  }
+
+  function day(month) {
+    if (month === 1) { return Math.floor(Math.random() * 30) + 1 }
+    if (month === 2) { return Math.floor(Math.random() * 28) + 1 }
+    if (month === 3) { return Math.floor(Math.random() * 30) + 1 }
+    // will add when April starts
+    //if (month === 4) { return Math.floor(Math.random() * (nowDate - 1)) + 1 }
+  }
+
+  function time(hourRange) {
+    var hours = Math.floor(Math.random() * hourRange - 1) + 1;
+    var minutes = Math.floor(Math.random() * nowMinutes - 1) + 1;
+    var ampm = '';
+    if (hours === 0) { ampm = 'am'; hours = 12 }
+    if (hours < 12) { ampm = 'am' }
+    if (hours === 12) { ampm = 'pm' }
+    if (hours > 12) { ampm = 'pm'; hours -= 12 }
+    if (minutes < 10) { minutes = '0' + minutes }
+    return hours + ':' + minutes + ampm;
+  }
+
+  function date() {
+    var randomMonth = month();
+    var randomDay = day(randomMonth);
+    var randomTime = '';
+    if (randomDay === now.getDate()) { randomTime = time(nowHours) }
+    else { randomTime = time(24) }
+    return randomMonth + '/' + randomDay + '/' + 2016 + ' ' + randomTime;
+  }
+
+  function tweet(who, quantity) {
+    quantity--
+    users[who].tweets.push({
+      user: who,
+      date: date(),
+      content: content(),
+      tags: [],
+      mentions: [],
+      retweet: []
+    })
+    if (quantity > 0) { tweet(who, quantity) }
+  }
+
+  function begin() {
+    for (prop in users) {
+      var quantity = Math.floor(Math.random() * 30) + 20;
+      tweet(prop, quantity);
+    }
+  }
+
+  return {
+    begin: begin,
+  }
+}
+
+module.exports = random();
---
+++ users.js
@@ -0,0 +1,238 @@
+var users = {
+  viethle126: {
+    login: {
+      email: 'viet@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Viet Le',
+    handle: 'viethle126',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  alysiasaquil: {
+    login: {
+      email: 'alysia@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Alysia Saquilabon',
+    handle: 'alysiasaquil',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  ajames91: {
+    login: {
+      email: 'andrew@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Andrew Velasquez',
+    handle: 'ajames91',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  arunpaulgopal: {
+    login: {
+      email: 'arun@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Arun Gopal',
+    handle: 'arunpaulgopal',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  bgnewuch: {
+    login: {
+      email: 'ben@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Benjamin Gnewuch',
+    handle: 'bgnewuch',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  brianwalen: {
+    login: {
+      email: 'brian@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Brian Walen',
+    handle: 'brianwalen',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  metcalfec: {
+    login: {
+      email: 'chris@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Chris Metcalfe',
+    handle: 'metcalfec',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  coreysl: {
+    login: {
+      email: 'corey@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Corey Lin ',
+    handle: 'coreysl',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  nanhyunchul: {
+    login: {
+      email: 'hyun@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Hyun Lee',
+    handle: 'nanhyunchul',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  jnguyen: {
+    login: {
+      email: 'justin@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Justin Nguyen',
+    handle: 'jnguyen',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  little78926: {
+    login: {
+      email: 'john@hoot.com',
+      password: 'hoot'
+    },
+    name: 'John Huang',
+    handle: 'little78926',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  mikefield: {
+    login: {
+      email: 'mike@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Michael Field',
+    handle: 'mikefield',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  treezrppl2: {
+    login: {
+      email: 'nathan@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Nathan Walston',
+    handle: 'treezrppl2',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  niralpokal: {
+    login: {
+      email: 'niral@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Niral Pokal',
+    handle: 'niralpokal',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  ronperris: {
+    login: {
+      email: 'ron@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Ron Perris',
+    handle: 'ronperris',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  espressohoarder: {
+    login: {
+      email: 'schlomo@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Schlomo Kofman',
+    handle: 'espressohoarder',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  tputs001: {
+    login: {
+      email: 'tom@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Tom Putsawatanachai',
+    handle: 'tputs001',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+  zane: {
+    login: {
+      email: 'zane@hoot.com',
+      password: 'hoot'
+    },
+    name: 'Zane DeFazio',
+    handle: 'zane',
+    tweets: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    favorites: [],
+  },
+}
+
+module.exports = users;
