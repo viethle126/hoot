eb289a540ddcd703aed6797647b62e0a8a56b884
Viet Le <viethle126@gmail.com>
2016-03-29 09:47:02 -0700

    Cards are created when visiting other timelines #2

--- public/js/default.js
+++ public/js/default.js
@@ -61,53 +61,160 @@ function addTweet(data, where) {
   timeline.appendChild(item);
 }
 
-function wantLine(user, where) {
+function wantLine(user, where, homeuser) {
   clear(where);
-  var data = { handle: user }
+  var data = { handle: user, home: homeuser }
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/timeline', true);
   xhr.setRequestHeader('Content-type', 'application/json');
   xhr.send(JSON.stringify(data));
 
   xhr.addEventListener('load', function() {
-    var tweets = JSON.parse(xhr.responseText);
-    console.log(tweets[0]);
-    tweets.reverse().forEach(function(element, index, array) {
-      addTweet(tweets[index], where);
-    })
+    var tweets = JSON.parse(xhr.responseText).reverse();
+    var displayed = tweets.length;
+    if (displayed > 50) { displayed = 50 } // show up to 50
+    for (var i = 0; i < displayed; i++) {
+      addTweet(tweets[i], where);
+    }
+  })
+}
+
+function card(data, where) {
+  var card = document.getElementById(where);
+  var imageDiv = elemClass('div', 'image');
+  var image = elemAttribute('img', 'src', 'images/' + data.handle + '.jpg');
+  var content = elemClass('div', 'content');
+  var header = elemClass('div', 'header');
+  var headerText = document.createTextNode(data.name);
+  var meta = elemClass('div', 'meta');
+  var handleSpan = elemClass('span', 'date');
+  var handleText = document.createTextNode('@' + data.handle);
+  var breakLine = document.createElement('br');
+  var countSpan = elemClass('span', 'date');
+  var write = elemClass('i', 'edit icon icon');
+  var writeText = document.createTextNode(data.tweets + ' Hoots');
+  var desc = elemClass('div', 'description');
+  var segment = elemClass('div', 'ui basic center aligned segment');
+  var follow = elemClass('button', 'ui basic blue button');
+  var add = elemClass('i', 'add user icon');
+  var unfollow = elemClass('button', 'ui basic blue  button');
+  var remove = elemClass('i', 'remove user icon');
+  var extra = elemClass('div', 'extra content');
+  var followingLink = document.createElement('a');
+  var following = elemClass('i', 'user icon');
+  var followingText = document.createTextNode(data.following);
+  var floated = elemClass('span', 'right floated');
+  var followerLink = document.createElement('a');
+  var follower = elemClass('i', 'users icon');
+  var followerText = document.createTextNode(data.followers);
+
+  followingLink.appendChild(following);
+  followingLink.appendChild(followingText)
+  followerLink.appendChild(follower);
+  followerLink.appendChild(followerText);
+  floated.appendChild(followerLink);
+  extra.appendChild(followingLink);
+  extra.appendChild(floated);
+  follow.appendChild(add);
+  follow.setAttribute('data-follow-text', 'Follow'); // use later
+  unfollow.appendChild(remove);
+  unfollow.setAttribute('data-follow-text', 'Unfollow'); // use later
+  segment.appendChild(follow);
+  segment.appendChild(unfollow);
+  if (data.handle !== data.me) { desc.appendChild(segment) }
+  handleSpan.appendChild(handleText);
+  countSpan.appendChild(write);
+  countSpan.appendChild(writeText);
+  meta.appendChild(handleSpan);
+  meta.appendChild(breakLine);
+  meta.appendChild(countSpan);
+  header.appendChild(headerText);
+  content.appendChild(header);
+  content.appendChild(meta);
+  content.appendChild(desc);
+  imageDiv.appendChild(image);
+  card.appendChild(imageDiv);
+  card.appendChild(content);
+  card.appendChild(extra);
+
+  if (data.follow === true) {
+    follow.classList.add('hidden');
+  } else {
+    unfollow.classList.add('hidden');
+  }
+}
+
+function wantCard(user, where, me) {
+  clear(where);
+  var data = { handle: user, home: me }
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/card', true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(JSON.stringify(data));
+
+  xhr.addEventListener('load', function() {
+    var content = JSON.parse(xhr.responseText);
+    card(content, where);
   })
 }
 
 document.getElementById('menu').addEventListener('click', function(e) {
-  if (e.target.id === 'home' || e.target.parentNode.id === 'home') {
+  var what = e.target
+  while (!what.id) {
+    what = what.parentNode;
+  }
+  if (what.id === 'home') {
     var home = document.getElementById('home');
     var yours = document.getElementById('your-timeline');
     var visit = document.getElementById('visit-timeline');
+    var you = document.getElementById('card');
+    var card = document.getElementById('visit-card');
 
     if (home.getAttribute('data-active') === 'false') {
       home.setAttribute('data-active', 'true');
       home.classList.add('active');
-      wantLine('viethle126', 'your-timeline');
+      yours.classList.remove('hidden');
+      wantLine('viethle126', 'your-timeline', true);
     }
     if (home.getAttribute('data-active') === 'visiting') {
       home.setAttribute('data-active', 'true');
       home.classList.add('active');
       visit.classList.add('hidden');
+      card.classList.add('hidden');
+      you.classList.remove('hidden');
       yours.classList.remove('hidden');
+      wantLine('viethle126', 'your-timeline', true);
     } else {
       return;
     }
   }
 });
 
-// testing timeline function when visiting other users' timelines
-document.getElementById('visit').addEventListener('click', function(e) {
+document.getElementById('visit-card');
+
+// temporary userlist, will move later
+document.getElementById('userlist').addEventListener('click', function(e) {
+  var list = document.getElementById('userlist');
   var home = document.getElementById('home');
   var yours = document.getElementById('your-timeline');
   var visit = document.getElementById('visit-timeline');
+  var you = document.getElementById('card')
+  var card = document.getElementById('visit-card');
+
+  var who = e.target;
+  while (!who.id) {
+    who = who.parentNode;
+  }
   home.setAttribute('data-active', 'visiting');
   home.classList.remove('active');
   yours.classList.add('hidden');
   visit.classList.remove('hidden');
-  wantLine('espressohoarder', 'visit-timeline');
-});
+  you.classList.add('hidden');
+  card.classList.remove('hidden');
+  wantLine(who.id, 'visit-timeline', false);
+  wantCard(who.id, 'visit-card', 'viethle126');
+})
+
+window.onload = function() {
+  wantCard('viethle126', 'card', 'viethle126');
+}
