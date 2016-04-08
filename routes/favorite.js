ce4ee97132b16acf0d7997c3fafb705ecdd3d79b
Viet Le <viethle126@gmail.com>
2016-04-07 21:25:18 -0700

    Added Router for Favorites #20

---
+++ routes/favorite.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var jSonParser = require('body-parser').json();
+// custom modules
+var users = require('../data/users');
+
+app.post('/add', jSonParser, function(req, res) {
+  var handle = req.body.handle;
+  var id = Number(req.body.id);
+  var me = req.body.home;
+  users[handle].tweets.forEach(function(element, index, array) {
+    if (element.id === id && users[me].favorites.indexOf(element) === -1) {
+      users[me].favorites.push(element);
+      return;
+    }
+  })
+  res.send();
+})
+
+app.post('/remove', jSonParser, function(req, res) {
+  var handle = req.body.handle;
+  var id = Number(req.body.id);
+  users[handle].favorites.forEach(function(element, index, array) {
+    if (element.id === id) {
+      array.splice(index, 1);
+      return;
+    }
+  })
+  res.send();
+})
+
+module.exports = router;
